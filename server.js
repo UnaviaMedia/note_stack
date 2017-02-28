7694ede4b308d016b4819caec821c7ff7d4effa5
UnaviaMedia <kroth@unaviamedia.ca>
2017-02-27 22:20:22 -0500

    Clean up unnecessary code

--- server.js
+++ server.js
@@ -1,15 +1,3 @@
 require('babel-register');
 
 require('./server.babel');
-
-/*var express = require("express");
-var app = express();
-
-app.get("/", function(req, res) {
-	res.send("Hi there");
-});
-
-// Start express server
-app.listen(3000, function() {
-	console.log("Node server listening on port 3000");
-});*/
