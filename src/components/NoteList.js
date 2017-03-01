2f0318830e86c01643dbf25179e31e4848bae96e
UnaviaMedia <kroth@unaviamedia.ca>
2017-02-28 22:59:14 -0500

    Minor refactoring

--- src/components/NoteList.js
+++ src/components/NoteList.js
@@ -2,19 +2,22 @@ import React from 'react';
 import Note from '../components/Note';
 
 const noteList = [
-	{ value: "Note 1" },
-	{ value: "Note 2" }
+	{ id: 1, value: "Note 1" },
+	{ id: 2, value: "Note 2" }
 ];
 
 class NoteList extends React.Component {
 	constructor() {
 		super();
+		this.state = {
+			list: noteList
+		}
 	}
 	render() {
 		return (
 			<ul>
-				{ noteList.map( (item, key) =>
-					<li key={key}><Note value={item.value} /></li>) }
+				{ this.state.list.map( note =>
+					<li key={note.id}><Note value={note.value} /></li>) }
 			</ul>
 		);
 	}
