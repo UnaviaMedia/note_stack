ed673b4a8734693d7da6361b40a1d1b155af59b0
UnaviaMedia <kroth@unaviamedia.ca>
2017-03-01 23:50:10 -0500

    Add Create and Update API endpoints

--- routes.js
+++ routes.js
@@ -1,4 +1,6 @@
 const express = require('express');
+
+//Initialize a router instance
 const router = express.Router();
 
 //Require database connection
@@ -14,29 +16,72 @@ router.get('/', function(req, res) {
 	let start = checkNumber(req.query.start);
 	let order = new Set(['asc', 'desc']).has(req.query.order) ? req.query.order : 'desc';
 
-	db.query('SELECT id, title, text FROM Note;', function(err, rows, fields) {
+	db.query('SELECT id, title, text FROM Note;', function(err, result, fields) {
 		if (err) {
 			//TODO: Handle errors
 			res.json({ msg: 'SELECT ERROR: Select failed', err: err });
+			return;
 		}
 
 		//TODO: Any validation/processing
 		res.json({
-			data: rows,
-			query: req.query,
+			msg: `SELECT: Select processed, ${result.length} rows returned`,
+			//query: req.query,
 			limit: limit,
 			start: start,
-			order: order
+			order: order,
+			data: result
 		});
 	});
 });
 
 router.post('/add', function(req, res) {
-	res.json({ msg: 'POST: Post request received' });
+	let title = req.body.title;
+	let text = req.body.text;
+
+	//TODO: Extend validation to include error messages
+	if (!title || !text) {
+		res.json({ msg: 'INSERT FAILED: Insert failed with invalid body' });
+		return;
+	}
+
+	db.query('INSERT INTO Note (title, text) VALUES (?, ?);', [title, text], function(err, result, fields) {
+		if (err) {
+			//TODO: Handle errors
+			res.json({ msg: 'INSERT ERROR: Insert failed', err: err });
+			return;
+		}
+
+		res.json({ msg: `INSERT: Insert processed, ${result.affectedRows} rows affected` });
+	});
 });
 
 router.put('/:id', function(req, res) {
-	res.json({ msg: `PUT: Update request received for id: ${req.params.id}` });
+	let id = checkNumber(req.params.id);
+	let title = req.body.title;
+	let text = req.body.text;
+
+	//Validate Note id
+	if (id <= 0) {
+		res.json({ msg: `UPDATE FAILED: Update failed with invalid id '${req.params.id}'` });
+		return;
+	}
+
+	//TODO: Extend validation to include error messages
+	if (!title || !text) {
+		res.json({ msg: 'UPDATE FAILED: Update failed with invalid body' });
+		return;
+	}
+
+	db.query('UPDATE Note SET title=?, text=? WHERE id=?;', [title, text, id], function(err, result, fields) {
+		if (err) {
+			//TODO: Handle errors
+			res.json({ msg: 'Update ERROR: Update failed', err: err });
+			return;
+		}
+
+		res.json({ msg: `UPDATE: Update processed, ${result.affectedRows} rows affected` });
+	});
 });
 
 router.delete('/:id', function(req, res) {
@@ -44,18 +89,19 @@ router.delete('/:id', function(req, res) {
 	let id = checkNumber(req.params.id);
 
 	if (id <= 0) {
-		res.json({ msg: `DELETE: Delete failed for invalid key '${req.params.id}'` });
+		res.json({ msg: `DELETE FAILED: Delete failed with invalid id '${req.params.id}'` });
 		return;
 	}
 
-	db.query('DELETE FROM Note WHERE id=?;', [id], function(err, rows, fields) {
+	db.query('DELETE FROM Note WHERE id=?;', [id], function(err, result, fields) {
 		if (err) {
 			//TODO: Handle errors
 			res.json({ msg: 'DELETE ERROR: Delete failed', err: err });
+			return;
 		}
 
 		//TODO: Handle no rows being deleted
-		res.json({ msg: `DELETE: Delete request for id: ${id}, ${rows.affectedRows} rows affected` });
+		res.json({ msg: `DELETE: Delete processed, ${result.affectedRows} rows affected` });
 	});
 });
 
