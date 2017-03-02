3902eba4995b760724fd11528d829210eaf9497b
UnaviaMedia <kroth@unaviamedia.ca>
2017-03-02 15:21:26 +0000

    Refactor connection and add initial opening

--- db.js
+++ db.js
@@ -1,14 +1,27 @@
 var mysql = require('mysql');
 
+const dbHost = process.env.IP || 'localhost';
+const dbUser = 'ns_api';
+
 //Create a mysql connection
 const db = mysql.createConnection({
-	host:		'localhost',
-	user:		'ns_api',
+	host:		dbHost,
+	user:		dbUser,
 	password:	'Passw0rD',
 	database:	'NoteStack',
 	debug: [
 		'ComQueryPacket', 'ResultSetHeaderPacket', 'RowDataPacket'
 	]
+});
+
+//Open a connection to the database
+db.connect(function(err) {
+	if (err) {
+		console.error(`Error connecting: ${err.stack}`);
+		return;
+	}	
+	
+	console.log(`Connected as id ${db.threadId}`);
 });
 
 module.exports = db;
