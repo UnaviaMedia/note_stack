ec4dac0f7f9b6b5f269a7871b52c09327bc30f24
UnaviaMedia <kroth@unaviamedia.ca>
2017-03-03 23:13:14 -0500

    Use ExtractText webpack plugin to create separate file

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const DIST_DIR = path.join(__dirname, 'dist');
 const CLIENT_DIR = path.join(__dirname, 'src');
@@ -13,22 +14,36 @@ module.exports = {
 	},
 	module: {
 		rules: [
+			//Process JS and JSX files with Babel for ES6 transpilation
 			{
 				test: /\.jsx?$/,
 				exclude: /node_modules/,
 				loader: 'babel-loader'
 			},
+			//Process SCSS files
 			{
 				test: /\.scss$/,
-				use: [
-					"style-loader",
-					"css-loader",
-					"sass-loader"
-				]
+				use: ExtractTextPlugin.extract({
+					fallback: 'style-loader',
+					use: [
+						'css-loader',
+						'sass-loader'
+					]
+				})
 			}
 		]
 	},
+	plugins: [
+		new ExtractTextPlugin({
+			//Name of the result file
+			filename: 'styles.css',
+			//Disables the plugin
+			disable: false
+		})
+	],
+	//Resolve enables locating a module by an attritube
 	resolve: {
-		extensions: ['.js', '.jsx']
+		//Extensions enables ommitting the following extensions when importing in ES6
+		extensions: ['.js', '.jsx', '.json']
 	}
 };
