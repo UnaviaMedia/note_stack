8ff0114b884d429c558a6d8a60b7bf4311378b6c
UnaviaMedia <kroth@unaviamedia.ca>
2017-03-03 20:24:38 -0500

    Fix incorrect timestamps

--- sql/load_data.sql
+++ sql/load_data.sql
@@ -2,10 +2,10 @@ USE NoteStack;
 
 -- Load some sample data
 INSERT INTO Note (title, content, dateCreated)
-VALUES ('Note 1', 'Text for Note 1', '2017-03-01T02:49:31.000Z');
+VALUES ('Note 1', 'Text for Note 1', STR_TO_DATE('2017-03-01 14:39:32', '%Y-%m-%d %H:%i:%s'));
 
 INSERT INTO Note (title, content, dateCreated)
-VALUES ('Note 2', 'Text for Note 2', '2017-03-02T12:24:59.000Z');
+VALUES ('Note 2', 'Text for Note 2', STR_TO_DATE('2017-03-02 12:24:59', '%Y-%m-%d %H:%i:%s'));
 
 INSERT INTO Note (title, content, dateCreated)
-VALUES ('Note 3', 'Text for Note 3', '2017-03-03T09:01:49.000Z');
+VALUES ('Note 3', 'Text for Note 3', STR_TO_DATE('2017-03-03 09:01:49', '%Y-%m-%d %H:%i:%s'));
