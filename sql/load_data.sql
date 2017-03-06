94cd40e6fbb4dd86db57d7e586d08658b73865a4
UnaviaMedia <kroth@unaviamedia.ca>
2017-03-06 18:52:54 -0500

    Fix bug with uuid not being updated in stored proc

--- sql/load_data.sql
+++ sql/load_data.sql
@@ -7,8 +7,8 @@ USE NoteStack;
 INSERT INTO Note (id, title, content, dateCreated)
 VALUES ('38f5dad0-a85e-4371-af75-6d67f8c9d14c', 'Note 1', 'Text for Note 1', STR_TO_DATE('2017-03-01 14:39:32', '%Y-%m-%d %H:%i:%s'));
 
-INSERT INTO Note (title, content, dateCreated)
+INSERT INTO Note (id, title, content, dateCreated)
 VALUES ('40ed55ad-12ba-45dc-b174-a4c41de52880', 'Note 2', 'Text for Note 2', STR_TO_DATE('2017-03-02 12:24:59', '%Y-%m-%d %H:%i:%s'));
 
-INSERT INTO Note (title, content, dateCreated)
+INSERT INTO Note (id, title, content, dateCreated)
 VALUES ('fbe4164c-eac2-46fb-bcb6-1e3151d47887', 'Note 3', 'Text for Note 3', STR_TO_DATE('2017-03-03 09:01:49', '%Y-%m-%d %H:%i:%s'));
